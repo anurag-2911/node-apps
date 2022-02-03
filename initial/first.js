32e5da098c5aa0272ff18060994a9138065692bb
Anurag Kumar <anurag.kumar@microfocus.com>
2022-02-03 09:33:52 +0530

    event handling

--- initial/first.js
+++ initial/first.js
@@ -9,3 +9,17 @@ http.createServer(function (req, res) {
   res.end();
   console.log('server started ');
 }).listen(443);
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var testEventHandler = function () {
+  console.log('recieved test event');
+}
+
+//Assign the eventhandler to an event:
+eventEmitter.on('testEvent', testEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('testEvent');
