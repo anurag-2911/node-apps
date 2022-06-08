14c5f3c34a671f0bb4e6f9d525ef0a089c70aad3
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-06-08 23:31:27 +0530

    fs inbuilt module

--- tutorial/src/builtInModules.js
+++ tutorial/src/builtInModules.js
@@ -17,9 +17,44 @@ const path = require('path');
 console.log(path.sep);
 
 
-const filePath = path.join('../content/','testdata','test.txt');
+const filePath = path.join('../content/', 'testdata', 'test.txt');
 console.log(filePath); // relative path
 console.log(path.basename(filePath)); // just the file name with extension
 
-const absolutePath = path.resolve(__dirname,'content','testdata','test.txt');
+const absolutePath = path.resolve(__dirname, 'content', 'testdata', 'test.txt');
 console.log(absolutePath);
+
+// fs module
+
+// fs sync functions
+const { readFileSync, writeFileSync, fstat } = require('fs');
+
+const readFilePath = path.join('../content', 'testdata', 'test.txt');
+const writeFilePath = path.join('../content', 'testdata', 'writefile01.txt');
+const filecontent = readFileSync(readFilePath, 'utf-8');
+writeFileSync(writeFilePath, `writing : ${filecontent}`);
+console.log(filecontent);
+const verifyWrite = readFileSync(writeFilePath, 'utf-8');
+console.log(verifyWrite);
+
+// fs async functions
+
+const { readFile, writeFile } = require('fs');
+
+readFile(readFilePath, (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(`async read result : ${data}`);
+});
+
+writeFile(writeFilePath,'hello async world',{'flag':'a'},(err,data)=>{
+if(err)
+{
+    console.log(err);
+    return;
+}
+console.log('write async ' + data);
+});
+
