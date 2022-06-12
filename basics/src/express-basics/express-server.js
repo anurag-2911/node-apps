a1e7af7f4d6757398745dfde72176e2de66faace
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-06-12 14:48:47 +0530

    public static folder and index.html

--- basics/src/express-basics/express-server.js
+++ basics/src/express-basics/express-server.js
@@ -1,9 +1,18 @@
 const express =  require('express');
+var path = require('path');
 
 const app = express();
 
+app.use(express.static('../../public'));
+
 app.get('/',(req,res)=>{
-    res.status(200).send('hello from my express server');
+    res.status(200).sendFile(path.join(__dirname, '../../content/pages/index.html'),(err)=>{
+        
+        if(err){
+            res.status(404).send('<h1>file not found</h1>');
+        }
+        
+    });
 })
 app.all('*',(req,res)=>{
     res.status(404).send('<h1>resource not found</h1>');
