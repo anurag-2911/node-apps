27a154d162481d5f52d40d8580d75f5f7cc20099
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-06-19 15:47:02 +0530

    sending json data.

---
+++ basics/content/testdata/data.js
@@ -0,0 +1,25 @@
+const product =[
+    {
+        id:1,
+        price:100,
+        name:'washing machine'
+    },
+    {
+        id:2,
+        price:150,
+        name:'iron'
+    }
+]
+
+const people =[
+    {
+        id:123,
+        name:'anurag'
+    },
+    {
+        id:234,
+        name:'kumar'
+    }
+]
+
+module.exports = {product,people};
--- basics/src/express-basics/express-server.js
+++ basics/src/express-basics/express-server.js
@@ -1,22 +1,32 @@
-const express =  require('express');
+const express = require('express');
+
 var path = require('path');
 
 const app = express();
 
 app.use(express.static('../../public'));
 
-app.get('/',(req,res)=>{
-    res.status(200).sendFile(path.join(__dirname, '../../content/pages/index.html'),(err)=>{
-        
-        if(err){
+app.get('/', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '../../content/pages/index.html'), (err) => {
+
+        if (err) {
             res.status(404).send('<h1>file not found</h1>');
         }
-        
+
     });
 })
-app.all('*',(req,res)=>{
+
+// returns a json object
+app.get('/json', (req, res) => {
+    const { product } = require('../../content/testdata/data');
+
+    res.json(product);
+});
+
+
+app.all('*', (req, res) => {
     res.status(404).send('<h1>resource not found</h1>');
 })
-app.listen(5555,()=>{
+app.listen(5555, () => {
     console.log('server is listening on port 5555');
 })
