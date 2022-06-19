4a8225e4ee72ce1d0abdf8f884caa6171b9a6abf
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-06-20 00:27:28 +0530

    request parameters

--- basics/src/express-basics/express-server.js
+++ basics/src/express-basics/express-server.js
@@ -1,28 +1,39 @@
 const express = require('express');
 
-var path = require('path');
-
 const app = express();
 
 app.use(express.static('../../public'));
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(path.join(__dirname, '../../content/pages/index.html'), (err) => {
-
-        if (err) {
-            res.status(404).send('<h1>file not found</h1>');
-        }
-
-    });
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
 })
+
+app.get('/api/products/', (req, res) => {
+    const { product } = require('../../content/testdata/data');
+    const p1 = product.map((product) => {
+        const { id, price, name } = product;
+        return { id, price, name };
+    })
+    res.json(p1);
+});
+
+app.get('/api/products/:pid', (req, res) => {
+    console.log('request parameters ' + JSON.stringify(req.params));
+    console.log('req param id ' + req.params.pid);
+
+    const { product } = require('../../content/testdata/data');
+    const p = product.find((product) => product.id === Number(req.params.pid))
+    if (!p) {
+        return res.status(404).send('<h1>product doesnt exist !</h1>');
+    }
+    res.json(p);
+});
 
 // returns a json object
 app.get('/json', (req, res) => {
     const { product } = require('../../content/testdata/data');
-
     res.json(product);
 });
-
 
 app.all('*', (req, res) => {
     res.status(404).send('<h1>resource not found</h1>');
